a14b1c39b967cc1906f311ebc88c1d8330c4c971
epandaashu-coder <e.pandaashu@gmail.com>
2025-12-24 14:55:12 +0530

    initial commit

---
+++ proj_test/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello Vite + React!</h1>
+      <a href="https://vitejs.dev/guide/features.html" target="_blank">
+        Documentation
+      </a>
+      <p>Code with vibe do not vibe-code</p>
+    </>
+  )
+}
+
+export default App
